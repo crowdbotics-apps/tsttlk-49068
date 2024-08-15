3b522b510d4b18012243853aeb9f95baae45ea1e
Vadym Kolpak <vadym@tuluko.eu>
2024-08-15 21:33:44 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tsttlk-49068",
-  url: "https://tsttlk-49068.botics.co",
-  api: "https://tsttlk-49068.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "tsttlk-49068",
+  "url": "https://tsttlk-49068.botics.co",
+  "api": "https://tsttlk-49068.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "about-the-app"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
