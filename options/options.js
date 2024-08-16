020d73ae03962acb37e35d57d12e3c45558a9d53
Vadym Kolpak <vadym@tuluko.eu>
2024-08-16 17:40:49 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -5,7 +5,7 @@ export const globalOptions = {
   "screenOptions": {
     "headerShown": false
   },
-  "initialRoute": "about-the-app"
+  "initialRoute": "Feedback1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
